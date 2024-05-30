99907e23ecfdeb824bf01e7734d735dc0bb725b7
vu nguyen <vunph64@gmail.com>
2024-05-30 17:18:58 +0700

    Update structure

--- projects/starter/e2e/src/app.e2e-spec.ts
+++ projects/starter/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-starter');
+    expect(await page.getTitleText()).toEqual('starter');
   });
 
   afterEach(async () => {
--- projects/starter/src/environments/environment.prod.ts
+++ projects/starter/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   application:
   {
-    name: 'angular-starter',
+    name: 'starter',
     angular: 'Angular 11.2.10',
     bootstrap: 'Bootstrap 4.6.0',
     fontawesome: 'Font Awesome 5.15.3',
